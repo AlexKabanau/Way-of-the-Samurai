0a9e04f9443066b1bc5b0568a2356267a3efc3b3
Alexandr Kabanau <Azraill1989@gmail.com>
2023-04-28 20:08:20 +0300

    add: add basic layout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,55 @@
-import Header from './Header';
-import Technologies from './Technologies';
+import React from 'react';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Technologies />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src="https://st2.depositphotos.com/4035913/6124/i/600/depositphotos_61243831-stock-photo-letter-s-logo.jpg" />
+      </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <main className='content'>
+        <div>
+          <img src='https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg'/>
+        </div>
+        <div>
+          avater+desc
+        </div>
+        <div>
+          my posts
+          <div>
+            new post
+          </div>
+          <div>
+            posts
+            <div>
+              Post1
+            </div>
+            <div>
+              Post2
+            </div>
+          </div>
+        </div>
+        Main content
+      </main>
+      <footer></footer>
     </div>
   );
 }
