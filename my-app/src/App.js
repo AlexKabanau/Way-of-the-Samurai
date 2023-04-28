3ab6a8e28dcce83aa3c572fb272f10a7229ca3b8
AlexKabanau <azraill1989@gmail.com>
2023-04-28 09:14:54 +0300

    feat: add inmport of componets

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import Header from './Header';
+import Technologies from './Technologies';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Technologies />
+    </div>
+  );
+}
+
+
+
+export default App;
