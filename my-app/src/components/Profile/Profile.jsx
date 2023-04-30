7b18dfd22e7f9027dc688849f62398f2f669b043
Alexandr Kabanau <Azraill1989@gmail.com>
2023-04-30 23:10:14 +0300

    feat: 15 folders structure

--- my-app/src/components/Profile/Profile.jsx
+++ my-app/src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from "./Profile.module.css"
+import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
   return (
@@ -10,21 +11,7 @@ const Profile = () => {
         <div>
           avatar+descriptions
         </div>
-        <div>
-          my posts
-          <div>
-            new post
-          </div>
-          <div>
-            posts
-            <div>
-              Post1
-            </div>
-            <div>
-              Post2
-            </div>
-          </div>
-        </div>
+        <MyPosts />
         Main content
       </main>
   )
