f13b5edf03432d9629bad7540593c6d1d1c1e8ff
AlexKabanau <azraill1989@gmail.com>
2023-05-05 13:38:38 +0300

    feat: 21 - dialogs

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -5,6 +5,34 @@ const Dialogs = (props) => {
   return (
     <div>
       Dialogs
+      <div className={s.dialogs}>
+        <div className={s.dialogsItems}>
+          <div className={s.dialog}>
+            Alex
+          </div>
+          <div className={`${s.dialog} ${s.active}`}>
+            Dima
+          </div>
+          <div className={s.dialog}>
+            Artiom
+          </div>
+          <div className={s.dialog}>
+            Vadzim
+          </div>
+          <div className={s.dialog}>
+            Dima
+          </div>
+          <div className={s.dialog}>
+            Vlad
+          </div>
+
+        </div>
+        <div className={s.messages}>
+          <div className={s.message}>Hi</div>
+          <div className={s.message}>Hi hi</div>
+          <div className={s.message}>WhatsApp</div>
+        </div>
+      </div>
     </div>
   )
 }
