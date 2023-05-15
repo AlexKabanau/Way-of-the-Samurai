8f3745f3cb3692ac3a1cfde725673f976f04c211
Alexandr Kabanau <Azraill1989@gmail.com>
2023-05-15 21:08:25 +0300

    feat: 28 remove data from components

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,59 +1,29 @@
 import React from "react"
 import s from "./Dialogs.module.css";
-import { NavLink } from "react-router-dom";
-
-
-const DialogItem = (props) => {
-  let path = '/dialogs/' + props.id;
-  return (
-    <div className={s.dialog}>
-      <NavLink to={path}>{props.name}</NavLink>
-    </div>
-  );
-}
-
-const Message = (props) => {
-  return (
-    <div className={s.message}>{props.message}</div>
-  )
-}
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 const Dialogs = (props) => {
 
-  let dialogsData = [
-    {id: 1, name: "Alex"},
-    {id: 2, name: "Dima"},
-    {id: 3, name: "Artiom"},
-    {id: 4, name: "Vadzim"},
-    {id: 5, name: "Dima"},
-    {id: 6, name: "Vlad"},
-  ];
+  let dialogsElements = props.dialogsData
+    .map( dialog => <DialogItem name={dialog.name} id={dialog.id} /> );
 
-  let messagesData = [
-    {id: 1, message: "Hi"},
-    {id: 2, message: "How are you"},
-    {id: 3, message: "How are you"},
-    {id: 4, message: "What's up"},
-    {id: 5, message: "What's up"},
-    {id: 6, message: "YO"},
-  ]; 
-
+  let messagesElements = props.messagesData
+    .map( message => <Message message={message.message} /> );
+  
   return (
     <div>
       Dialogs
       <div className={s.dialogs}>
         <div className={s.dialogsItems}>
-          <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-          <DialogItem name="Dima" id="2" />
-          <DialogItem name="Artiom" id="3" />
-          <DialogItem name="Vadzim" id="4" />
-          <DialogItem name="Dima" id="5" />
-          <DialogItem name="Vlad" id="6" />
+          {
+            dialogsElements
+          }
         </div>
         <div className={s.messages}>
-          <Message message={messagesData[0].message}/>
-          <Message message="How are you"/>
-          <Message message="What's up"/>
+          {
+            messagesElements
+          }
         </div>
       </div>
     </div>
--- my-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import s from "./MyPosts.module.css"
 import Post from "./Post/Post";
 
-const MyPosts = () => {
+const MyPosts = (props) => {
 
-  let postsData = [
-    {id: 1, message: "Hi, how are you?", name: "Alex K", age: 34, likesCount: 12},
-    {id: 2, message: "It's my first post", name: "John Doe", age: 52, likesCount: 11},
-    {id: 3, message: "Lorem", name: "Dima K", age: 28, likesCount: 8},
-  ]; 
+  // let posts = [
+  //   {id: 1, message: "Hi, how are you?", name: "Alex K", age: 34, likesCount: 12},
+  //   {id: 2, message: "It's my first post", name: "John Doe", age: 52, likesCount: 11},
+  //   {id: 3, message: "Lorem", name: "Dima K", age: 28, likesCount: 8},
+  // ];
+
+  let postsElements = props.posts
+  .map( post => <Post name={post.name} id={post.id} age={post.age} likeCount={post.likesCount}/> );
+
 
   return (
     <div className={s.postsBlock}>
@@ -26,10 +30,9 @@ const MyPosts = () => {
       </div>
       <div className={s.posts}>
         posts
-        <Post message={postsData[0].message} name={postsData[0].name} age={postsData[0].age} likeCount={postsData[0].likesCount} />
-        <Post message={postsData[1].message} name={postsData[1].name} age={postsData[1].age} likeCount={postsData[1].likesCount} />
-        <Post message={postsData[2].message} name={postsData[2].name} age={postsData[2].age} likeCount={postsData[2].likesCount} />
-        
+        {
+          postsElements
+        }
 
       </div>
     </div>
