164c72fb6e301e479c11748ac861e7526472db4e
AlexKabanau <azraill1989@gmail.com>
2023-08-24 15:49:05 +0300

    Updates

---
+++ precache-manifest.8709cf7b0f565d7e2bf0265beb7c4c2c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "3663c0f83cbebe887161",
+    "url": "/Way-of-the-Samurai/my-app/static/css/main.545c202b.chunk.css"
+  },
+  {
+    "revision": "3663c0f83cbebe887161",
+    "url": "/Way-of-the-Samurai/my-app/static/js/main.3663c0f8.chunk.js"
+  },
+  {
+    "revision": "d8f5ba55c3b78c25f0a5",
+    "url": "/Way-of-the-Samurai/my-app/static/js/1.d8f5ba55.chunk.js"
+  },
+  {
+    "revision": "95948aaee3daee728f3d",
+    "url": "/Way-of-the-Samurai/my-app/static/css/2.5796747e.chunk.css"
+  },
+  {
+    "revision": "95948aaee3daee728f3d",
+    "url": "/Way-of-the-Samurai/my-app/static/js/2.95948aae.chunk.js"
+  },
+  {
+    "revision": "461a8a3cec53a674161e",
+    "url": "/Way-of-the-Samurai/my-app/static/js/3.461a8a3c.chunk.js"
+  },
+  {
+    "revision": "f936fd3097cf8077ac40",
+    "url": "/Way-of-the-Samurai/my-app/static/js/runtime~main.f936fd30.js"
+  },
+  {
+    "revision": "462deb47014ae425ba57f4e403c3a3e9",
+    "url": "/Way-of-the-Samurai/my-app/static/media/feed_FILL0_wght400_GRAD0_opsz48.462deb47.svg"
+  },
+  {
+    "revision": "9922fce36153093b9ff26048dcbb1331",
+    "url": "/Way-of-the-Samurai/my-app/static/media/person_FILL0_wght400_GRAD0_opsz48.9922fce3.svg"
+  },
+  {
+    "revision": "5c8d9cb8bd472a635f571d499fa0a1f8",
+    "url": "/Way-of-the-Samurai/my-app/static/media/chat_FILL0_wght400_GRAD0_opsz48.5c8d9cb8.svg"
+  },
+  {
+    "revision": "5532c41bcda6924eebf1deb6c94b7a51",
+    "url": "/Way-of-the-Samurai/my-app/static/media/library_music_FILL0_wght400_GRAD0_opsz48.5532c41b.svg"
+  },
+  {
+    "revision": "7661be01a3e5053d656aeebed991ff0b",
+    "url": "/Way-of-the-Samurai/my-app/static/media/users-svgrepo-com.7661be01.svg"
+  },
+  {
+    "revision": "b3fc9306c91855370c7874c619f9fcae",
+    "url": "/Way-of-the-Samurai/my-app/static/media/settings_FILL0_wght400_GRAD0_opsz48.b3fc9306.svg"
+  },
+  {
+    "revision": "0736889957197707903acfbe00cf0d7d",
+    "url": "/Way-of-the-Samurai/my-app/static/media/Spinner-1s-200px.07368899.svg"
+  },
+  {
+    "revision": "4e6d996b6c1fcf1519dce126fe732c57",
+    "url": "/Way-of-the-Samurai/my-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Way-of-the-Samurai/my-app/precache-manifest.8709cf7b0f565d7e2bf0265beb7c4c2c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Way-of-the-Samurai/my-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
