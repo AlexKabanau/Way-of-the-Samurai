947770115d92580f76c688d6b5ccf1aad18f613a
Alexandr Kabanau <Azraill1989@gmail.com>
2023-08-31 18:09:06 +0300

    Updates

---
+++ precache-manifest.84fb2c54faed89ed2a2e1eb053c6d698.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "00c6fe4779e14e3eef73",
+    "url": "/Way-of-the-Samurai/static/js/0.00c6fe47.chunk.js"
+  },
+  {
+    "revision": "b05c7a2ad9541f985fef",
+    "url": "/Way-of-the-Samurai/static/css/main.d165445c.chunk.css"
+  },
+  {
+    "revision": "b05c7a2ad9541f985fef",
+    "url": "/Way-of-the-Samurai/static/js/main.b05c7a2a.chunk.js"
+  },
+  {
+    "revision": "79624a268f5f82c756d9",
+    "url": "/Way-of-the-Samurai/static/js/2.79624a26.chunk.js"
+  },
+  {
+    "revision": "0b06eed56dad78933f8d",
+    "url": "/Way-of-the-Samurai/static/css/3.2310d59b.chunk.css"
+  },
+  {
+    "revision": "0b06eed56dad78933f8d",
+    "url": "/Way-of-the-Samurai/static/js/3.0b06eed5.chunk.js"
+  },
+  {
+    "revision": "13274e3184a2040cdfbd",
+    "url": "/Way-of-the-Samurai/static/css/4.9fe47bd8.chunk.css"
+  },
+  {
+    "revision": "13274e3184a2040cdfbd",
+    "url": "/Way-of-the-Samurai/static/js/4.13274e31.chunk.js"
+  },
+  {
+    "revision": "325b1a3546929707bb2c",
+    "url": "/Way-of-the-Samurai/static/js/5.325b1a35.chunk.js"
+  },
+  {
+    "revision": "635c6c75731f4eab3a81",
+    "url": "/Way-of-the-Samurai/static/js/runtime~main.635c6c75.js"
+  },
+  {
+    "revision": "5c8d9cb8bd472a635f571d499fa0a1f8",
+    "url": "/Way-of-the-Samurai/static/media/chat_FILL0_wght400_GRAD0_opsz48.5c8d9cb8.svg"
+  },
+  {
+    "revision": "9922fce36153093b9ff26048dcbb1331",
+    "url": "/Way-of-the-Samurai/static/media/person_FILL0_wght400_GRAD0_opsz48.9922fce3.svg"
+  },
+  {
+    "revision": "462deb47014ae425ba57f4e403c3a3e9",
+    "url": "/Way-of-the-Samurai/static/media/feed_FILL0_wght400_GRAD0_opsz48.462deb47.svg"
+  },
+  {
+    "revision": "b3fc9306c91855370c7874c619f9fcae",
+    "url": "/Way-of-the-Samurai/static/media/settings_FILL0_wght400_GRAD0_opsz48.b3fc9306.svg"
+  },
+  {
+    "revision": "5532c41bcda6924eebf1deb6c94b7a51",
+    "url": "/Way-of-the-Samurai/static/media/library_music_FILL0_wght400_GRAD0_opsz48.5532c41b.svg"
+  },
+  {
+    "revision": "7661be01a3e5053d656aeebed991ff0b",
+    "url": "/Way-of-the-Samurai/static/media/users-svgrepo-com.7661be01.svg"
+  },
+  {
+    "revision": "0736889957197707903acfbe00cf0d7d",
+    "url": "/Way-of-the-Samurai/static/media/Spinner-1s-200px.07368899.svg"
+  },
+  {
+    "revision": "f57fbd72f8d376e889adf842084e73d2",
+    "url": "/Way-of-the-Samurai/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Way-of-the-Samurai/precache-manifest.2c34199ca55115e2fd583388ca557fc3.js"
+  "/Way-of-the-Samurai/precache-manifest.84fb2c54faed89ed2a2e1eb053c6d698.js"
 );
 
 workbox.clientsClaim();
