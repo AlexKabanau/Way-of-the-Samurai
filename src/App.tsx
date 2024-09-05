770c1f0273a87634539f7091f75d55e3ade5b1a1
Alexandr Kabanau <azraill1989@gmail.com>
2024-09-05 14:35:43 +0300

    add: implement ant Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { FC, Suspense } from 'react';
+import React, { FC, Suspense, useState } from 'react';
 // import Header from './components/Header/Header';
-import HeaderContainer from './components/Header/HeaderContainer';
+import {AppHeader} from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 // import Profile from './components/Profile/Profile';
 import { UsersPage } from './components/Users/UsersContainer';
@@ -9,7 +9,7 @@ import './App.css';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Setting';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter, NavLink, Link } from 'react-router-dom';
 // import ProfileContainer from './components/Profile/ProfileContainer';
 import { LoginPage } from './components/Login/Login';
 // import { getAuthUserData } from './redux/auth-reducer';
@@ -20,9 +20,23 @@ import Preloader from './components/Common/Preloader/Preloader';
 import store, { AppStateType } from './redux/redux-store';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Avatar, Breadcrumb, Layout, Menu, theme } from 'antd';
+import SubMenu from 'antd/es/menu/SubMenu';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+const { Header, Content, Footer, Sider } = Layout;
 
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer_copy_2'));
+
+
+// const {
+//   token: { colorBgContainer, borderRadiusLG },
+// } = theme.useToken();
+
 
 type MapPropsType = ReturnType<typeof mapStateToProps>;
 type DispatchPropsType = {
@@ -30,7 +44,6 @@ type DispatchPropsType = {
 };
 
 class App extends React.Component<MapPropsType & DispatchPropsType> {
-  // class App = (props) => {
   catchAllUnhandledErrors = (e: PromiseRejectionEvent) => {
     alert('Some Error occured 1');
   };
@@ -43,33 +56,112 @@ class App extends React.Component<MapPropsType & DispatchPropsType> {
   }
 
   render() {
-    if (!this.props.initialized) {
-      return <Preloader />;
-    }
+    // if (!this.props.initialized) {
+    //   return <Preloader />;
+    // }
     return (
-      // <BrowserRouter>
-      <div className="app-wrapper">
-        <HeaderContainer />
-        <Navbar />
-        <div className="app-wrapper-content">
-          <Suspense fallback={<Preloader />}>
-            <Routes>
-              <Route path="/" element={<ProfileContainer />} />
-              <Route path="/dialogs/*" element={<DialogsContainer />} />
-              <Route path="/profile/" element={<ProfileContainer />} />
-              <Route path="/profile/:userId" element={<ProfileContainer />} />
-              <Route path="/news" element={<News />} />
-              <Route path="/music" element={<Music />} />
-              <Route path="/settings" element={<Settings />} />
-              {/* <Route path="/users" element={<UsersPage />} /> */}
-              <Route path="/users" element={<UsersPage pageTitle="Самураи" />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="*" element={<div>404 NOT FOUND</div>} />
+    <Layout>
+      <AppHeader/>
+      <Content style={{ padding: '0 48px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Layout
+          // style={{ padding: '24px 0', background: colorBgContainer, borderRadius: borderRadiusLG 
+
+          // }}
+        >
+          <Sider 
+          // style={{ background: colorBgContainer }} width={200}
+          >
+            <Menu
+              mode="inline"
+              // defaultSelectedKeys={['1']}
+              // defaultOpenKeys={['sub1']}
+              // selectedKeys={[current]}
+              // onClick={onClick}
+              style={{ height: '100%' }}
+              // items={items2}
+            >
+              <SubMenu key='sub1' icon={<UserOutlined />} title='My Profile'>
+                <Menu.Item key='1'><Link to="/profile">Profile</Link></Menu.Item>
+                <Menu.Item key='2'><Link to="/dialogs">Dialogs</Link></Menu.Item>
+                <Menu.Item key='3'>Option 1</Menu.Item>
+                <Menu.Item key='4'>Option 2</Menu.Item>
+              </SubMenu>
+              <SubMenu key='sub2' icon={<LaptopOutlined />} title='Users'>
+                <Menu.Item key='5'><Link to="/users">Developers</Link></Menu.Item>
+                <Menu.Item key='6'>Option 3</Menu.Item>
+                <Menu.Item key='7'>Option 4</Menu.Item>
+              </SubMenu>
+              <Menu.Item key='8'><Link to="/news">News</Link></Menu.Item>
+              <Menu.Item key='9'><Link to="/music">Music</Link></Menu.Item>
+              <Menu.Item key='10'><Link to="/settings">Settings</Link></Menu.Item>
+              <Menu.Item key='11'>Option 5</Menu.Item>
+            </Menu>
+          </Sider>
+          <Content style={{ padding: '0 24px', minHeight: 280 }}>
+          <Routes>
+      //         <Route path="/" element={<ProfileContainer />} />
+      //         <Route path="/dialogs/*" element={<DialogsContainer />} />
+      //         <Route path="/profile/" element={<ProfileContainer />} />
+      //         <Route path="/profile/:userId" element={<ProfileContainer />} />
+      //         <Route path="/news" element={<News />} />
+      //         <Route path="/music" element={<Music />} />
+      //         <Route path="/settings" element={<Settings />} />
+      //         <Route path="/users" element={<UsersPage pageTitle="Самураи" />} />
+      //         <Route path="/login" element={<LoginPage />} />
+      //         <Route
+                path="*"
+                element={
+                  <div>
+                    404 NOT FOUND
+                  </div>
+                }
+              />
             </Routes>
-          </Suspense>
-        </div>
-        <footer></footer>
-      </div>
+            </Content>
+        </Layout>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Samuray Social Network created by it-kamasutra.by
+      </Footer>
+    </Layout>
+    
+      // <BrowserRouter>
+      
+      // <div className="app-wrapper">
+      //   <HeaderContainer />
+      //   <Navbar />
+      //   <div className="app-wrapper-content">
+      //     <Suspense fallback={<Preloader />}>
+      //       <Routes>
+      //         <Route path="/" element={<ProfileContainer />} />
+      //         <Route path="/dialogs/*" element={<DialogsContainer />} />
+      //         <Route path="/profile/" element={<ProfileContainer />} />
+      //         <Route path="/profile/:userId" element={<ProfileContainer />} />
+      //         <Route path="/news" element={<News />} />
+      //         <Route path="/music" element={<Music />} />
+      //         <Route path="/settings" element={<Settings />} />
+      //         {/* <Route path="/users" element={<UsersPage />} /> */}
+      //         <Route path="/users" element={<UsersPage pageTitle="Самураи" />} />
+      //         <Route path="/login" element={<LoginPage />} />
+      //         <Route
+      //           path="*"
+      //           element={
+      //             <div>
+      //               404 NOT FOUND
+      //               <Button type="dashed">ok</Button>
+      //             </div>
+      //           }
+      //         />
+      //       </Routes>
+      //     </Suspense>
+      //   </div>
+      //   <footer></footer>
+      // </div>
       // </BrowserRouter>
     );
   }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,42 @@
 import React from 'react';
 import s from './Header.module.css';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { Avatar, Button, Menu } from 'antd';
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Header } from 'antd/es/layout/layout';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectCurrentUserLogin, selectIsAuth } from '../../redux/auth-selectors';
+import { AppDispatch } from '../../redux/redux-store';
+import { logout } from '../../redux/auth-reducer';
 
-export type MapPropsType = {
-  isAuth: boolean;
-  login: string | null;
-};
-export type DispatchPropsType = {
-  logout: () => void;
-};
-
-const Header: React.FC<MapPropsType & DispatchPropsType> = (props) => {
+export const AppHeader: React.FC = (props) => {
   // debugger
+  const isAuth = useSelector(selectIsAuth);
+  const login = useSelector(selectCurrentUserLogin);
+
+  const dispatch: AppDispatch = useDispatch();
+
+  const logoutCallback = () => {
+    dispatch(logout());
+  };
   return (
-    <header className={s.header}>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/The_social_network.svg/2560px-The_social_network.svg.png"
-        alt="Social networks"
-      />
-      <div className={s.loginBlock}>
-        {props.isAuth ? (
-          <div>
-            {props.login} - <button onClick={props.logout}>LogOut</button>
-          </div>
-        ) : (
+    <Header style={{ display: 'flex', alignItems: 'center' }}>
+      <div className="demo-logo" />
+      <Menu theme="dark" mode="horizontal" style={{ flex: 1, minWidth: 0 }}>
+        <Menu.Item key="1">
+          <Link to="/profile">Profile</Link>
+        </Menu.Item>
+      </Menu>
+      {isAuth ? (
+        <div>
+          <Avatar alt={login || ''} shape="square" size={64} icon={<UserOutlined />} />
+          {login} <Button onClick={logoutCallback}>LogOut</Button>
+        </div>
+      ) : (
+        <Button>
           <NavLink to={'/login'}>Login</NavLink>
-        )}
-      </div>
-    </header>
+        </Button>
+      )}
+    </Header>
   );
 };
-
-export default Header;
